e612a86f6a898d70198840267ff03418ad7111a4
João Ricardo <32642843+joaoricardao@users.noreply.github.com>
2019-01-22 11:53:30 -0300

    Create tipos_variaveis.cpp

---
+++ tipos_variaveis.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   string nome="Paulo";
+   int alunoA=30;
+   double nota=7.5;
+   char turma='A';
+   bool ingressante=true;
+   
+   cout<<"O nome do aluno é: "<<nome<<endl;
+   cout<<"O número do aluno é: "<<alunoA<<endl;
+   cout<<"A nota do aluno é: "<<nota<<endl;
+   cout<<"A turma do aluno é: "<<turma<<endl;
+   cout<<"Situação do aluno (1-ingressante, 0-veterano): "<<ingressante<<endl;
+   
+   return 0;
+}
