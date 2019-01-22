81dcd6eb9d5b9a20269372a02eea007d57f68c05
João Ricardo <32642843+joaoricardao@users.noreply.github.com>
2019-01-22 12:08:44 -0300

    Create 01-operadores.cpp

---
+++ 01-operadores.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   double n;
+   cout<<"Por favor, informe um valor em ponto flutuante: ";
+   cin>>n;
+   cout<<"n == "<<n<<endl;
+   cout<<"n+1 == "<<n+1<<endl;
+   cout<<"O dobro de n == "<<2*n<<endl;
+   cout<<"O triplo de n == "<<3*n<<endl;
+   cout<<"Metade do n == "<<n/2<<endl;
+   cout<<"N ao quadrado == "<<n*n<<endl;
+   
+   return 0;
+}
