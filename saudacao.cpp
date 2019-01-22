228b6bb57da89e07b2a95010b82d957ceec60212
João Ricardo <32642843+joaoricardao@users.noreply.github.com>
2019-01-22 11:29:24 -0300

    Create saudacao.cpp

---
+++ saudacao.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nome;
+    cout<<"Por favor, insira seu primeiro nome (seguido de 'enter'): ";
+    cin>>nome;
+    cout << "Olá, " << nome<< "!";
+    return 0;
+}
